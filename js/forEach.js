b81b008444214f4553d785e714d27ae4c17d4ab4
Natalie Shif <natalia.shif@gmail.com>
2024-05-02 15:01:23 +0300

    questionnaire explained

--- js/forEach.js
+++ js/forEach.js
@@ -11,7 +11,20 @@ nums1.forEach((num,idx) => {
 /** MISSION: 
  *  Use nums1.map() and create new numeric array 
  * comprised of the old elements divided by their indexes
- * (except of 0)
+ * (except of 0 - return just the number itself)
  */
+
+// const nums2 = nums1.map((num,idx) => {
+//     return idx === 0 ? num : num/idx;
+// })
+
+// We could shorten our ARROW function:
+// 1. If there is only one argument - we could remove the brackets
+// 2. If there is only 1 command inside the ARROW function body, we could
+//     rid of the curly brackets 
+// 3. Without curly brackets it performs "return" anyway - so we should 
+//    remove "return" also.
+
+const nums2 = nums1.map((num,idx) => idx === 0 ? num : num/idx )
 
 
